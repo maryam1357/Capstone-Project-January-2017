27bd765f8debd28da6b35b3f4cc3f3ed6f671532
Maryam Veisi <maryam@Maryams-MacBook-Pro.local>
2017-02-02 01:41:27 -0800

    wednsday night

--- Code/AalenAdditiveModel.py
+++ Code/AalenAdditiveModel.py
@@ -6,7 +6,7 @@ from lifelines import AalenAdditiveFitter
 '''This module contains the basicz functions for
     1. Instantiating aalen object from the AalenAdditiveFitter class.
     2. Performing bootstrapping on the dataset.
-    3. Using the trained bootstrap models to predict the lifetime ('Total_years') of users.
+    3. Using the trained bootstrap models to predict the lifetime ('days') of users.
     4. Using the trained bootstrap models to create the cummulative hazard functions for users.
     '''
 
@@ -17,8 +17,8 @@ def Aalen_model(df, l2 = 0.01, coeff_pen = 0.1, smooth_pen = 0.1):
 	i.e., the hazard rate is a linear function of the covariates.
 
     Parameters
-    df: Pandas dataframe.  The y column must be called "Total_years."  A column of Boolean values called
-        "censored" to indicate which row of data is censored, as indicated by True or False or 1 or 0.
+    df: Pandas dataframe.  The y column must be called "days."  A column of Boolean values called
+        "kac" to indicate which row of data is kac, as indicated by True or False or 1 or 0.
     coeff_pen = 0.1: Attach a L2 penalizer to the size of the coeffcients during regression. This improves
         stability of the estimates and controls for high correlation between covariates.  For example,
         this shrinks the absolute value of c_{i,t}. Recommended, even if a small value.
@@ -31,12 +31,12 @@ def Aalen_model(df, l2 = 0.01, coeff_pen = 0.1, smooth_pen = 0.1):
 
     Output: aaf instance fitted to df'''
     aaf = AalenAdditiveFitter(fit_intercept=False, coef_penalizer=coeff_pen, smoothing_penalizer=smooth_pen, nn_cumulative_hazard=True)
-    aaf.fit(df, 'Total_years', event_col='censored')
+    aaf.fit(df, 'days', event_col='kac')
     return aaf
 
 def Bootstrap(df, bootstrap_count = 100):
-    '''Accepts a pandas dataframe with a column called 'Total_years' which is y and
-    a column called 'censored' which indicates which column of data is censored.
+    '''Accepts a pandas dataframe with a column called 'days' which is y and
+    a column called 'kac' which indicates which column of data is kac.
 
     Performs bootstrap fitting with the Aalen model on a dataset df.
     Draws n=number of rows samples from df with replacement. Trains a Aalen model by invoking the Aalen_model
@@ -44,7 +44,7 @@ def Bootstrap(df, bootstrap_count = 100):
     Returns a list of bootstrap_count of Aalen models each trained by a bootstrapped set of data.
 
     Parameters:
-    df: A pandas dataframe with y = 'Total_years' and censored data = 'censored' and the rest of the columns are the features.
+    df: A pandas dataframe with y = 'days' and kac data = 'kac' and the rest of the columns are the features.
     bootstrap_count:  Number of bootstrapped model to train and append to the AAF_list
 
     Output: A list of length bootstrap_count of trained Aalen models.
@@ -61,13 +61,13 @@ def Bootstrap(df, bootstrap_count = 100):
 def Aalen_predict_lifetimes(AAF_list, test_dataset):
     '''Accepts a list (AAF_list) containing m trained bootstrapped AAF models (trained by bootstrapping in the Bootstrap function) and a test data set (test_dataset).  Calculate the predicted lifetimes by each model for each user.   Outputs the mean and median of lifetime for each user.
 
-    The overall scheme is to iterate through the m bootstrap models and generate m predictions and m hazard functions for each row of data.  The mean and median of the predictions are calculated and added back to the the dataset as new columns called 'Mean_Pred_Total_years' and 'Med_Pred_Total_years'
+    The overall scheme is to iterate through the m bootstrap models and generate m predictions and m hazard functions for each row of data.  The mean and median of the predictions are calculated and added back to the the dataset as new columns called 'Mean_Pred_days' and 'Med_Pred_days'
 
     Parameters:
     AAF_list: A list of trained models to make predictions based of the test_dataset
-    test_dataset: A pandas dataframe with y column = 'Total_years'
+    test_dataset: A pandas dataframe with y column = 'days'
 
-    Output:  test_dataset with two columns 'Mean_Pred_Total_years' and 'Med_Pred_Total_years' added.
+    Output:  test_dataset with two columns 'Mean_Pred_days' and 'Med_Pred_days' added.
     '''
     aaf_predict = []
 
@@ -79,8 +79,8 @@ def Aalen_predict_lifetimes(AAF_list, test_dataset):
 
     #Calculation of mean and median of aaf_predict
     aaf_pred_array = np.asarray(aaf_predict)
-    test_dataset['Mean_Pred_Total_years'] = np.mean(aaf_pred_array, axis = 0)
-    test_dataset['Med_Pred_Total_years'] = np.median(aaf_pred_array, axis = 0)
+    test_dataset['Mean_Pred_days'] = np.mean(aaf_pred_array, axis = 0)
+    test_dataset['Med_Pred_days'] = np.median(aaf_pred_array, axis = 0)
 
     return test_dataset
 
@@ -93,7 +93,7 @@ def Aalen_cum_haz(AAF_list, test_dataset):
 
     Parameters:
     AAF_list: A list of trained models to make predictions
-    test_dataset: A pandas dataframe with y column = 'Total_years'
+    test_dataset: A pandas dataframe with y column = 'days'
 
     Output:
     aaf_cum_haz:  A list of dataframes each presenting the hazard functions of the models in AAF_list.
@@ -147,11 +147,11 @@ def plot_cum_haz_functions(AAF_list, x_max =40):
 
 def plot_user_cum_haz(user_cum_haz_functions_list, number_of_users = 8, years = 5, y_max = 5, lw = 0.01):
     '''Accepts a list of (bootstrap) trained models, calculates each of their cumulative hazard functions,
-	randomly selects number_of_users (censored), and plots their hazard functions.
+	randomly selects number_of_users (kac), and plots their hazard functions.
 
     Parameters:
-	'Total_years' represents how many years the users have been active.
-	'censored' indicates whether a user is still active (True = active user).
+	'days' represents how many years the users have been active.
+	'kac' indicates whether a user is still active (True = active user).
 
     Output:
     A plot of the cumulative hazard functions for the randomly selected users.
@@ -161,25 +161,24 @@ def plot_user_cum_haz(user_cum_haz_functions_list, number_of_users = 8, years = 
 
 	#This list of colors will be cycled through during the plotting.  If the number_of_users > 8, the colors
 	#will have to be repeated.
-	colors = ['grey', 'lightgreen', 'blue','red', 'magenta', 'gold', 'green', 'orange']
+    colors = ['grey', 'lightgreen', 'blue','red', 'magenta', 'gold', 'green', 'orange']
 
 	#Randomly selects a number_of_users from the user_dataset to be plotted.
-	rows = random.sample(range(user_cum_haz_functions_list[0].shape[1]), number_of_users)
+    rows = random.sample(range(user_cum_haz_functions_list[0].shape[1]), number_of_users)
 
 	#Iterates through the rows (users selected) and then through all cumulative hazard functions
 	#to plot them on the same figure.  Note that this order of iteraton must be maintained
 	#so that the cum hazard function for each user is plotted with the same color.
-	for i, row in enumerate(rows):
+    for i, row in enumerate(rows):
 		print row
 		for user_cum_haz_functions in user_cum_haz_functions_list:
 			plt.plot(user_cum_haz_functions.T.iloc[row], alpha = lw, color = colors[i%8])
-	plt.xlabel('Years', size = 10)
-	plt.ylabel("user cumulative hazard function", size = 10)
-	text = 'Cumulative hazards for ' + str(number_of_users) + ' random censored users.'
-	plt.annotate(text, xy=(0, 0), xytext=(0.2, 2.8))
-	plt.grid()
-	plt.xlim(0, years)
-	plt.ylim(0, y_max)
-	plt.show()
-	plt.close()
-	return
+    plt.xlabel('Years', size = 10)
+    plt.ylabel("user cumulative hazard function", size = 10)
+    text = 'Cumulative hazards for ' + str(number_of_users) + ' random kac users.'
+    plt.annotate(text, xy=(0, 0), xytext=(0.2, 2.8))
+    plt.grid()
+    plt.xlim(0, years)
+    plt.ylim(0, y_max)
+    plt.show()
+    plt.close()
--- Code/EDA.py
+++ Code/EDA.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
+import lifelines as sa
 from lifelines import KaplanMeierFitter
+import pandas as pd
 
-def plot_Kaplan_Meier_overall(user_dataset):
+def plot_Kaplan_Meier_overall(df_final_km):
 	'''Accepts a dataframe of user data.  Plots the overall Kaplan-Meier curve based of the lifetime of the users.  The active users ('censored') will be excluded from the plot.
 
 	Parameters:
-	user_dataset: Pandas dataframe which contain at least the columns 'Total-years' and 'censored'.  'Total_years' represents how many years the users have been active.  'censored' indicates whether a user is still active (True = active user).
+	df_final_km: Pandas dataframe which contain at least the columns 'Total-years' and 'censored'.  'Total_years' represents how many years the users have been active.  'censored' indicates whether a user is still active (True = active user).
 
 	Output:
 	A Kaplan-Meier plot.
@@ -19,8 +21,9 @@ def plot_Kaplan_Meier_overall(user_dataset):
 	#whether the user is censored (not churned).  Only user who
 	#has churned (not censored) are used because we don't know the
 	#'Total_years' of users who have not churned yet.
-	T = user_dataset['Total_years']
-	C = user_dataset['censored']
+	print df_final_km.head()
+	T = df_final_km['days']
+	C = df_final_km['kac']
 
 	#Create KaplanMeierInstance
 	kmf = KaplanMeierFitter()
@@ -30,49 +33,119 @@ def plot_Kaplan_Meier_overall(user_dataset):
 	fig = plt.figure(figsize=(5, 5))
 	ax = fig.add_subplot(111)
 	kmf.plot(ax=ax)
-	ax.set_xlabel('Years', size = 20)
+	ax.set_xlabel('days', size = 20)
 	ax.set_ylabel('Surviving user population', size = 20)
-	ax.set_xlim(0,40)
+	ax.set_xlim(0,2000)
 	ax.set_ylim(0, 1)
 	ax.grid()
 	ax.legend(loc = 'best', fontsize = 20)
 	plt.show()
 	return
 
-def plot_Kaplan_Meier_feature(user_dataset):
-    '''Accepts a dataframe of user data.  For each feature (column), it plots the Kaplan-Meier curves of the users based on whether the feature is true or false.  The active users ('censored') will be excluded from the plot.
+def plot_Kaplan_Meier_feature(df_final_km):
+	'''Accepts a dataframe of user data.  For each feature (column), it plots the Kaplan-Meier curves of the users based on whether the feature is true or false.  The active users ('censored') will be excluded from the plot.
 
-    Parameters:
-    user_dataset: Pandas dataframe which contain at least the columns 'Total-years' and 'censored'.  'Total_years' represents how many years the users have been active.  'censored' indicates whether a user is still active (True = active user).
+	Parameters:
+	df_final_km: Pandas dataframe which contain at least the columns 'Total-years' and 'censored'.  'Total_years' represents how many years the users have been active.  'censored' indicates whether a user is still active (True = active user).
 
-    Output:
-    Kaplan-Meier plot(s).
+	Output:
+	Kaplan-Meier plot(s).
 
-    This function does not return anything.
-    '''
-    T = user_dataset['Total_years']
-    C = user_dataset['censored']
-    features = list(user_dataset.columns)
-    features.remove('Total_years')
-    features.remove('censored')
-    features.remove('Baseline')
-    kmf = KaplanMeierFitter()
-    for feature in features:
-        Above_mean = user_dataset[feature] > user_dataset[user_dataset['censored'] == 0][feature].mean()
-        fig = plt.figure(figsize=(5, 5))
-        ax = fig.add_subplot(111)
-        kmf = KaplanMeierFitter()
-        kmf.fit(T[Above_mean], C[Above_mean], label = feature + ': Yes or > mean')
-        kmf.plot(ax=ax, linewidth = 2)
-        kmf.fit(T[~Above_mean], C[~Above_mean], label = feature + ': No or < mean')
-        kmf.plot(ax=ax, linewidth = 2)
-        ax.set_xlabel('Years', size = 10)
-        ax.set_ylabel('Surviving user population', size = 10)
-        ax.set_xlim(0,40)
-        ax.set_ylim(0, 1)
-        ax.grid()
-        ax.legend(loc = 'upper right', fontsize = 10)
-        plt.show()
+	This function does not return anything.
+	'''
+	print df_final_km.head()
+	df_final_km_dropped = df_final_km.drop(['f_type_0','f_type_1','f_type_2','f_type_3','f_type_5'], axis=1)
+	features = list(df_final_km_dropped.columns)
+	features.remove('days')
+	features.remove('kac')
+	features.remove('count_initial_scan_subscriptions')
+	features.remove('kac_quarter')
+	print features
+	for feature in features:
+		ax = plt.subplot(111)
+		for item in df_final_km_dropped[feature].unique():
+			df_sub = df_final_km_dropped.loc[df_final_km_dropped[feature]==item]
+			km = sa.KaplanMeierFitter()
+			km.fit(durations=df_sub['days'], event_observed=df_sub['kac'], label=item)
+			ax = km.plot(ax=ax)
+		plt.show()
 
-def clean_data()
-    return df_final
+def assign_email_group(string):
+    if string.split('.')[-1]=='edu':
+        return 'edu'
+    elif string.startswith('gmail'):
+        return 'gmail'
+    else:
+        return 'others'
+
+def clean_user_data(df_user):
+	print "Cleaning user data"
+	df_user['email_domain']=df_user['email'].str.split('@').str.get(1)
+	df_user['domains'] = df_user.email_domain.apply(assign_email_group)
+	df_clean_user = df_user.drop(['email_domain','email','user_referral_detail','user_referral_id','user_referral_value','disconnect_timestamp','slice_user_id','kac_history','goodness_score','country','region','city','metro_code'], axis=1)
+	print df_clean_user.head()
+	return df_clean_user
+
+def clean_filter(df_filter):
+	print "Cleaning filter"
+	df_filter_type_count = df_filter.groupby(['user_id', 'filter_type_id'])['filter_type_id'].count().unstack().fillna(0)
+	df_filter_type_count = df_filter_type_count.rename(columns={0:'f_type_0', 1:'f_type_1', 2:'f_type_2', 3:'f_type_3', 5:'f_type_5'})
+	print df_filter_type_count.head()
+	return df_filter_type_count
+
+def merge_and_clean_user_history_data(df_clean_user, df_history):
+	print "Merging and cleaning user and history data"
+	df_clean_histroy = pd.DataFrame(df_history.groupby('user_id')['disconnect_timestamp'].max())
+	df_clean_user_history = df_clean_user.join(df_clean_histroy, on='user_id')
+	#df_user_history['active_time'] = pd.to_datetime(df_user_history.kac_update_timestamp) - pd.to_datetime(df_user_history.disconnect_timestamp)
+	#df_user_history['not_active'] = (df_user_history.active_time.astype(int)==0).astype(int)
+	df_clean_user_history = df_clean_user_history[df_clean_user_history['kac'].isin([1,3])]
+	df_clean_user_history['kac_update_timestamp'] = pd.to_datetime(df_clean_user_history['kac_update_timestamp'])
+	df_clean_user_history['join_timestamp'] = pd.to_datetime(df_clean_user_history['join_timestamp'])
+	df_clean_user_history['days'] = (df_clean_user_history.kac_update_timestamp - df_clean_user_history.join_timestamp)
+	df_clean_user_history['days'] = df_clean_user_history.days.dt.days
+	df_clean_user_history['kac_quarter'] = df_clean_user_history.kac_update_timestamp.dt.quarter
+	df_clean_user_history.kac = df_clean_user_history.kac.map({3: 1, 1: 0})
+	df_clean_user_history['intial_scan_count'] = pd.cut(df_clean_user_history.count_initial_scan_subscriptions, bins=[-1, 355, 999999], labels=['<mean', '>mean'])
+	print df_clean_user_history.head()
+	return df_clean_user_history
+
+def create_final_set(df_clean_user_history, df_filter_type_count):
+	print "Creating the final km data set"
+	df_final_km = df_clean_user_history.join(df_filter_type_count, on='user_id')
+	df_final_km[['f_type_0', 'f_type_1', 'f_type_2', 'f_type_3', 'f_type_5']] = df_final_km[['f_type_0', 'f_type_1', 'f_type_2', 'f_type_3', 'f_type_5']].fillna(0)
+	df_final_km['rollup'] = pd.cut(df_final_km.f_type_0, bins=[-1, 20, 999999], labels=['<mean', '>mean'])
+	df_final_km['unsubscribe'] = pd.cut(df_final_km.f_type_1, bins=[-1, 105, 999999], labels=['<mean', '>mean'])
+	df_final_km['inbox'] = pd.cut(df_final_km.f_type_2, bins=[-1, 45, 999999], labels=['<mean', '>mean'])
+	df_final_km['fresh'] = pd.cut(df_final_km.f_type_3, bins=[-1, 185, 999999], labels=['<mean', '>mean'])
+	df_final_km = df_final_km.drop(['join_timestamp','kac_update_timestamp','disconnect_timestamp'], axis=1)
+	a = df_final_km['user_id']
+	df_final_km.set_index(a,inplace=True)
+	df_final_km.drop('user_id', axis=1, inplace=True)
+	print df_final_km.head()
+	return df_final_km
+
+def clean_data_km(userData, historyData, filterData):
+	print "Cleaning km data set"
+	df_user = pd.read_csv(userData, delimiter=';')
+	df_history = pd.read_csv(historyData, delimiter=';')
+	df_filter = pd.read_csv(filterData,delimiter=';')
+	df_clean_user = clean_user_data(df_user)
+	df_clean_user_history = merge_and_clean_user_history_data(df_clean_user, df_history)
+	df_filter_type_count = clean_filter(df_filter)
+	return create_final_set(df_clean_user_history, df_filter_type_count)
+
+def clean_data(userData, historyData, filterData):
+	print "Cleaning data"
+	df_final_km = clean_data_km(userData, historyData, filterData)
+	dummies_signup_origin = pd.get_dummies(df_final_km['domains']).rename(columns = lambda x: 'domains_'+str(x))
+	df_final_km = pd.concat([df_final_km,dummies_signup_origin],axis=1)
+
+	dummies_signup_origin = pd.get_dummies(df_final_km['signup_origin']).rename(columns = lambda x: 'signup_origin_'+str(x))
+	df_final_km = pd.concat([df_final_km,dummies_signup_origin],axis=1)
+	df_final_km = df_final_km.drop('signup_origin_Old Website', 1)
+	df_final_aa = df_final_km.drop(['domains_edu', 'signup_origin','domains','rollup', 'unsubscribe', 'inbox', 'fresh','intial_scan_count'], axis=1)
+
+	print "Cleaning data completed!!!"
+	print df_final_aa.head()
+	return df_final_aa
